ecb53ed4659968eb15db7a1f5b046dda04cf2f74
Mon Lado <duyosoft@gmail.com>
2022-11-23 20:08:16 +0100

    cambios producción

---
+++ README.md
@@ -0,0 +1,18 @@
+**************************************
+         Aplicación de Tareas
+**************************************
+
+Para instalar la aplicación se debe crear un archivo
+con el nombre .env en la ruta raíz 
+con los siguientes datos:
+------------------------------------------------------------------
+SECRET_KEY = <'valor de la clave secreta'>
+
+MYSQL_HOST = <'nombre del host donde está la base de datos'>
+MYSQL_DB = <'nombre de la base de datos'>
+MYSQL_USER = <'nombre usuario de la base de datos'>
+MYSQL_PASSWORD = <'contraseña de la base de datos'>
+MYSQL_PORT = <'puerto de la base de datos'>
+------------------------------------------------------------------
+
+sustituir los valores entre símbolos <' '> por los valores correspondientes indicados
--- tareas/settings.py
+++ tareas/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z7@%x%ou6#6-#*k(j6pxle#qsch4+fekw0!nfy&$3)3#38b&l='
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,13 +76,22 @@ WSGI_APPLICATION = 'tareas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('MYSQL_DB'),
+        'USER': config('MYSQL_USER'),
+        'PASSWORD': config('MYSQL_PASSWORD'),
+        'HOST': config('MYSQL_HOST'),
+        'PORT': config('MYSQL_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -117,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 LOGIN_URL = '/login'
 
